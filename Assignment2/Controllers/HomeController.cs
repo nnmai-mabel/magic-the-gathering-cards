e42aee8508bce01cb053f46801aabace7b97788e
Mai Nguyen <nnmai0606@gmail.com>
2023-11-19 10:02:13 +1030

    Initial commit

---
+++ Assignment2/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Assignment2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "All Cards";
+            return View();
+        }
+
+        public ActionResult Details()
+        {
+            ViewBag.Title = "Card Details";
+            return View();
+        }
+
+        public ActionResult UserDecks()
+        {
+            ViewBag.Title = "User Decks";
+            return View();
+        }
+
+        public ActionResult DeckDisplay()
+        {
+            ViewBag.Title = "Deck DIsplay";
+            return View();
+        }
+    }
+}
---
+++ Assignment2/Controllers/ManagementController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Assignment2.Controllers
+{
+    public class ManagementController : Controller
+    {
+        // GET: Management
+        public ActionResult CreateDeck()
+        {
+            ViewBag.Title = "Create Decks";
+            return View();
+        }
+    }
+}
